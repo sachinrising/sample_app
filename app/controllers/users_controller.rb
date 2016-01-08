44e0519b55a6224993792bda3133c9b5ea466b3c
sachin srivastava <sachinrising@gmail.com>
2016-01-08 20:42:40 +0000

    user micro post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def create
@@ -64,14 +65,15 @@ class UsersController < ApplicationController
     
     # Before filters
     
+    # Below method moved to ApplicationController
     # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    # def logged_in_user
+    #   unless logged_in?
+    #     store_location
+    #     flash[:danger] = "Please log in."
+    #     redirect_to login_url
+    #   end
+    # end
     
     def correct_user
       @user = User.find(params[:id])
